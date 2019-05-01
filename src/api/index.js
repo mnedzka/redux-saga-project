9865373aa37a252c0e997d71e1fb23aa701266ef
Michal Nedzka <michael.nedzka@gmail.com>
2019-05-01 19:55:12 +0200

    Handle statistics for images in order to practice sagas more

--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,4 @@
+// get your own key from unsplash please 😇
 const KEY = '?client_id=5f96323678d05ff0c4eb264ef184556868e303b32a2db88ecbf15746e6f25e02';
 const URL = `https://api.unsplash.com/photos/`;
 
@@ -10,4 +11,13 @@ const fetchImages = async page => {
   return data;
 };
 
-export { fetchImages };
+const fetchImageStats = async id => {
+  const response = await fetch(`${URL}/${id}/statistics${KEY}`);
+  const data = await response.json();
+  if (response.status >= 400) {
+    throw new Error(data.errors);
+  }
+  return data;
+};
+
+export { fetchImages, fetchImageStats };
